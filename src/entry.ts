a6351a01182d548a8e791ab2758cebf7436ff133
Georgy Marchuk <gmarcuk@gmail.com>
2021-07-12 02:47:06 +0200

    add license comment

--- src/entry.ts
+++ src/entry.ts
@@ -1,3 +1,9 @@
+/** @license Blobity
+ * Copyright (c) Georgy Marchuk.
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // this is here for webpack to expose Blobity as window.Blobity
 import Blobity from './index'
 module.exports = Blobity
