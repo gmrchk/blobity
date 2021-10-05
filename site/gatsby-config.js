842793f93e4fbb3199eca0cf21085efcf3d709bc
Georgy Marchuk <gmarcuk@gmail.com>
2021-10-05 20:47:47 +0200

    google analytics

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: `https://blobity.dev`,
     },
-    plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-styled-components',
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: 'G-91DWBNMP88',
+            },
+        },
+    ],
 };
