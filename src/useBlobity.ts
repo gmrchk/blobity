4cbed72917845a27b8ebeb3adb79d18870509183
Georgy Marchuk <gmarcuk@gmail.com>
2021-10-31 19:59:51 +0100

    dont duplicate useBlobity react hook

--- src/useBlobity.ts
+++ src/useBlobity.ts
@@ -1,14 +1,1 @@
-import Blobity, { Options } from './Blobity';
-import { useEffect, useRef } from 'react';
-
-export default (options: Partial<Options>) => {
-    const instance = useRef<Blobity | null>(null);
-
-    useEffect(() => {
-        if (!instance.current) {
-            instance.current = new Blobity(options);
-        }
-    }, []);
-
-    return instance;
-};
+export { default } from './react/useBlobity';
